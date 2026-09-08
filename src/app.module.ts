de2d8a96e6b7314e443c9f8748f2bb64d5576127
Dušan Mlynarčík <dusan.mlynarcik@email.cz>
2026-09-08 19:31:58 +0200

    register pipe provider in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
+import { APP_PIPE } from '@nestjs/core';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import AuthenticationModule from './authentication/module.js';
@@ -6,6 +7,9 @@ import AuthenticationModule from './authentication/module.js';
 @Module({
   imports: [AuthenticationModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    { provide: APP_PIPE, useValue: new ValidationPipe({ whitelist: true }) },
+  ],
 })
 export class AppModule {}
