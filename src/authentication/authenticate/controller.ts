a4e4e5709d9d5b5df8071ea63f5f8f5fa2faf7d5
Dušan Mlynarčík <dusan.mlynarcik@email.cz>
2026-09-09 15:59:11 +0200

    polishing

--- src/authentication/authenticate/controller.ts
+++ src/authentication/authenticate/controller.ts
@@ -2,13 +2,13 @@ import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import AuthenticationAuthenticateUseCase from './use-case.js';
 import { AuthenticationAuthenticateRequest } from '../requests.js';
 
-@Controller('authentications/authenticate')
+@Controller('authentications')
 export default class AuthenticationAuthenticateController {
   constructor(
     private readonly authenticateUseCase: AuthenticationAuthenticateUseCase,
   ) {}
 
-  @Post()
+  @Post('authenticate')
   @HttpCode(HttpStatus.OK)
   async authenticate(@Body() request: AuthenticationAuthenticateRequest) {
     const token = await this.authenticateUseCase.execute(request.magicToken);
