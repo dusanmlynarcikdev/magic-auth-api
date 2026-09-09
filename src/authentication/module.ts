27e9316fca80b5f3f21070138654c2088df9c0b1
Dušan Mlynarčík <dusan.mlynarcik@email.cz>
2026-09-09 14:11:26 +0200

    Add authenticate endpoint

--- src/authentication/module.ts
+++ src/authentication/module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import AuthenticateController from './authenticate.controller.js';
+import AuthenticateUseCase from './authenticate.use-case.js';
 import AuthenticationCreateController from './create.controller.js';
 import AuthenticationCreateUseCase from './create.use-case.js';
 import AuthenticationRepository from './repository.js';
@@ -6,8 +8,9 @@ import ClockProvider from '../clock.provider.js';
 import TokenProvider from '../token.provider.js';
 
 @Module({
-  controllers: [AuthenticationCreateController],
+  controllers: [AuthenticateController, AuthenticationCreateController],
   providers: [
+    AuthenticateUseCase,
     AuthenticationCreateUseCase,
     AuthenticationRepository,
     ClockProvider,
--- src/authentication/requests.ts
+++ src/authentication/requests.ts
@@ -1,5 +1,11 @@
 import { IsNotEmpty, IsString } from 'class-validator';
 
+export class AuthenticateRequest {
+  @IsString()
+  @IsNotEmpty()
+  readonly magicToken!: string;
+}
+
 export class AuthenticationCreateRequest {
   @IsString()
   @IsNotEmpty()
