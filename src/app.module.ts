fbe8b7d051aff00e8a039f4ce8587791cb4b03af
Dušan Mlynarčík <dusan.mlynarcik@email.cz>
2026-09-11 09:55:02 +0200

    Add authentications expired delete task

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module, ValidationPipe } from '@nestjs/common';
 import { APP_PIPE } from '@nestjs/core';
+import { ScheduleModule } from '@nestjs/schedule';
 import HealthController from './health.controller.js';
 import AuthenticationModule from './authentication/module.js';
 
 @Module({
-  imports: [AuthenticationModule],
+  imports: [AuthenticationModule, ScheduleModule.forRoot()],
   controllers: [HealthController],
   providers: [
     { provide: APP_PIPE, useValue: new ValidationPipe({ whitelist: true }) },
---
+++ test/functional/authentication/delete/expired.task.spec.ts
@@ -0,0 +1,22 @@
+import AuthenticationDeleteExpiredTask from '../../../../src/authentication/delete/expired/task.js';
+import AuthenticationDeleteExpiredUseCase from '../../../../src/authentication/delete/expired/use-case.js';
+import AuthenticationFactory from '../../../support/authentication/factory.js';
+import AuthenticationQuery from '../../../support/authentication/query.js';
+import AuthenticationRepository from '../../../../src/authentication/repository.js';
+import ClockProvider from '../../../../src/clock.provider.js';
+
+describe('AuthenticationDeleteExpiredTask', () => {
+  const repository = new AuthenticationRepository();
+  const useCase = new AuthenticationDeleteExpiredUseCase(repository, {
+    now: () => new Date('2026-09-04T12:40:46.000Z'),
+  } satisfies ClockProvider);
+  const task = new AuthenticationDeleteExpiredTask(useCase);
+
+  it('delete', async () => {
+    await repository.add(AuthenticationFactory.create());
+
+    await task.execute();
+
+    expect(await AuthenticationQuery.findAll()).toHaveLength(0);
+  });
+});
